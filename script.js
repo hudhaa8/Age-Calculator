9b1af2286ed6d7c1d3ec0e814c928a5a8efcb1df
hudhaa8 <90915199+hudhaa8@users.noreply.github.com>
2021-12-16 22:45:46 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+var dateelement = document.createElement('input');
+dateelement.setAttribute('type', 'date');
+dateelement.setAttribute('id', 'date');
+document.body.append(dateelement);
+
+var button = document.createElement('button');
+button.innerHTML = "Display data";
+button.className = "btn btn-primary";
+button.setAttribute('type', 'button');
+button.addEventListener('click', displaydata);
+document.body.append(button);
+
+
+function displaydata() {
+    let input = document.getElementById("date").value;
+    if (Date.parse(input)) {
+        var inputdate = new Date(input);
+        var currentdate = new Date();
+        
+        var millisecdiff = currentdate.getTime() - inputdate.getTime();
+        console.log(millisecdiff);
+
+        var seconddiff = Math.floor(millisecdiff / 1000);
+        console.log(seconddiff);
+
+        var minutediff = Math.floor(seconddiff / 60);
+        console.log(minutediff);
+        var hoursdiff = Math.floor(minutediff / 60);
+        console.log(hoursdiff);
+
+        var daydiff = Math.floor(hoursdiff / 24);
+        console.log(daydiff);
+
+        var yeardiff = currentdate.getFullYear() - inputdate.getFullYear();
+        console.log(yeardiff);
+
+        console.log(currentdate.getMonth());
+        var monthdiff = (yeardiff * 12) + (currentdate.getMonth() - inputdate.getMonth());
+        console.log(monthdiff);
+
+    }
+    else {
+        console.log("give a proper valid date")
+    }
+
+
+    var res = document.createElement('div');
+    res.innerHTML = `Given date to Current Time Difference in :<br>
+     Milliseconds :${millisecdiff}<br>
+     Seconds: ${seconddiff}<br>
+    Minutes: ${minutediff}<br> 
+    Hours: ${hoursdiff}<br>
+    Day :${daydiff}<br> 
+    Month:${monthdiff}<br>
+    Year :${yeardiff}`
+     document.body.append(res)
+}
